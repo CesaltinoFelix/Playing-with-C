9f0c0310712e207e9c76a8abdea1ca545db800fe
cesaltinofelix <cesaltinofelix2000@gmail.com>
2024-07-10 14:58:26 +0100

    ok

--- test.c
+++ test.c
@@ -10,10 +10,10 @@ void difine_value(int *valores)
 void print_value(int *valores)
 {
 	int i = 0;
-	printf("&valores = %p\n", &valores);
-	while(valores[i])
+	printf("&valores = %p\n\n", &valores);
+	while(i < 5)
 	{
-		printf("valores = %p | *value %d", &valores[i], valores[i]);
+		printf("valores = %p | *value %d\n", &valores[i], valores[i]);
 		i++;
 	}
 }
