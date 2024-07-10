05ac954eefdca9f3794eea58228d044c2b9c355c
cesaltinofelix <cesaltinofelix2000@gmail.com>
2024-07-10 14:45:07 +0100

    updating code

--- test.c
+++ test.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_value(int *valores)
+{
+	int i = 0;
+	printf("&valores = %p\n", &valores);
+	while(valores[i])
+	{
+		printf("valores = %p | *value %d", valores, *valores);
+	}
+}
 
 int main(void)
 {
-	printf("Ola, eu sou o cesaltino!!!");   
+	int *valores = NULL;
+
+	valores = (int *)malloc(sizeof(int) * 5);
+
+	int i = -1;
+	while(++i < 5)
+	{
+		valores[i] = i;
+	}
+	print_value(valores);
 }
