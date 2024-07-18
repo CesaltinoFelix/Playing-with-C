27f742bb6e8dcd83239ddac73a5b45f418b3462c
CesaltinoFelix <65049617+CesaltinoFelix@users.noreply.github.com>
2024-07-18 15:45:59 +0100

    Update crud00.c

--- cruds/crud00.c
+++ cruds/crud00.c
@@ -25,35 +25,59 @@ Book *create_book(char *title, unsigned int pages, int price)
 
     return (new_book);
 }
+Book *copy_book(Book *book)
+{
+    return (create_book(book->title, book->pages, book->price));
+}
 
 Student *create_student(char *name, int age, Book *book)
 {
-    (void)name;
-    (void)age;
-    (void)book;
+    Student *student = (Student *)malloc(sizeof(Student));
+    
+    ft_strcpy(student->name ,name);
+    student->fav_book = copy_book(book);
+    student->age = age;
 
-    return (NULL);
+    return (student);
 }
 
 void destroy_book(Book **book)
 {
     free(*book);
     *book = NULL;
+} 
+
+void destroy_student(Student **student)
+{
+    destroy_book(&(*student)->fav_book);
+    free(*student);
+    *student = NULL;
 }
 
 void print_book(Book *book)
 {
-    ft_printf("BOOK INFORMATION:\n");
-    ft_printf("-----------------------\n");
-    ft_printf("Title: %s\nPages: %d\nPrice %d\n", book->title, book->pages, book->price);
+    ft_printf("\tBOOK INFORMATION:\n");
+    ft_printf("\t-----------------------\n");
+    ft_printf("\t\tTitle: %s\n\t\tPages: %d\n\t\tPrice %d\n", book->title, book->pages, book->price);
 }
+
+void print_student(Student *student)
+{
+    ft_printf("STUDENT INFORMATION:\n");
+    ft_printf("-----------------------\n");
+    ft_printf("Name: %s\nAge: %d\n", student->name, student->age);
+    print_book(student->fav_book);
+
+}
+
 
 int main(void)
 {
     Book *book = create_book("A era do gelo", 200, 1999);
     Student *student = create_student("Cesaltino Felix", 24, book);
     
-    print_book(book);
+    print_student(student);
 
+    destroy_student(&student);
     destroy_book(&book);    
 }
