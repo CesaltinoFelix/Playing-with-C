7f1966468d3e894856217d01a7a9c60a7a934025
cesaltinofelix <cesaltinofelix2000@gmail.com>
2024-12-15 19:57:44 +0000

    updating code

--- minishell/main.c
+++ minishell/main.c
@@ -1,85 +1,4 @@
-#include <stdio.h>
-#include <readline/readline.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <readline/history.h>
-
-static int	count_words(const char *s, char delimiter)
-{
-	size_t	words;
-
-	words = 0;
-	if (*s == '\0')
-		return (words);
-	if (delimiter == '\0')
-		return (1);
-	while (*s == delimiter && *s)
-		s++;
-	while (*s)
-	{
-		words++;
-		while (*s != delimiter && *s)
-			s++;
-		while (*s == delimiter && *s)
-			s++;
-	}
-	return (words);
-}
-
-void	*rollback(char **morsels)
-{
-	while (*morsels)
-		free(*morsels);
-	free(morsels);
-	return (NULL);
-}
-
-char	**write_words(char **morsels, const char *s, char delimiter)
-{
-	size_t	length;
-	size_t	i;
-
-	i = 0;
-	while (*s)
-	{
-		length = 0;
-		while (s[length] != delimiter && s[length])
-			length++;
-		morsels[i] = (char *) malloc(length * sizeof(char) + 1);
-		if (morsels[i] == NULL)
-			return (rollback(morsels));
-		morsels[i][length] = '\0';
-		while (length > 0)
-		{
-			morsels[i][length - 1] = s[length - 1];
-			length--;
-		}
-		while (*s != delimiter && *s)
-			s++;
-		while (*s == delimiter && *s)
-			s++;
-		i++;
-	}
-	return (morsels);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**arr;
-	size_t	word_count;
-
-	word_count = count_words(s, c);
-	arr = (char **) malloc((word_count + 1) * sizeof(char *));
-	if (arr == NULL)
-		return (NULL);
-	while (*s == c && *s)
-		s++;
-	arr = write_words(arr, s, c);
-	arr[word_count] = NULL;
-	return (arr);
-}
+#include "minishell.h"
 
 int main(void)
 {
@@ -95,7 +14,7 @@ int main(void)
 		str = readline("minishell1> ");
 		if (str == NULL)
 		{
-			printf("\nExiting...\n");
+			ft_printf("\nExiting...\n");
 			break;
 		}
 		add_history(str);
@@ -120,7 +39,7 @@ int main(void)
             }
             else if (strcmp(argument_list[0], "pwd") == 0)
             {
-                printf("%s\n", getcwd(s, sizeof(s)));
+                ft_printf("%s\n", getcwd(s, sizeof(s)));
             }
             else
             {
---
+++ minishell/minishell.h
@@ -0,0 +1,15 @@
+#ifndef MINISHELL_H
+
+#define MINISHELL_H
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <readline/history.h>
+#include "printf.h"
+#include "libft.h"
+
+#endif
